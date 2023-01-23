a39853a98edab2e09a0e7b9de05e7faff8e355aa
Friska Sianturi <friska.lsianturi@gmail.com>
2023-01-23 21:42:02 +0700

    test: add tc for manage category apis

--- tests/Unit/CategoryTest.php
+++ tests/Unit/CategoryTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Category;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
@@ -14,7 +15,46 @@ class CategoryTest extends TestCase
     public function test_create_category_success()
     {
         $response = $this->post('/admin/categories',['name'=>'Science']);    
+
         $response->assertStatus(200);
+    }
+
+    public function test_show_category_success(){
+        $category = Category::factory()->create();
+
+        $response = $this->get('/admin/categories/' . $category->id);
+
+        $category->delete();
+        
+        $response->assertStatus(200);
+    }
+
+    public function test_update_category_success(){
+
+        $category = Category::factory()->create();
+
+        $name_update = 'Science update';
+
+        $response = $this->put('/admin/categories/'.$category->id, ['name'=> $name_update]);
+
+        $category->refresh();
+
+        $response->assertStatus(200);
+
+        $this->assertEquals($name_update, $category->name);
+
+        $category->delete();
+    }
+
+    public function test_delete_category_success(){
+        $category = Category::factory()->create();
+
+        $response = $this->delete('/admin/categories/' . $category->id);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseMissing('categories', ['id' => $category->id, 'name' => $category->name]);
+
 
     }
 }
