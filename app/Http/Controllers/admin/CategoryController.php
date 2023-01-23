f785803399d64638c0668949f556a5ba1a147eba
Friska Sianturi <friska.lsianturi@gmail.com>
2023-01-23 21:41:36 +0700

    feat: add apis for managing category

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -9,8 +9,31 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function store(StoreRequest $request){
+    public function store(StoreRequest $request)
+    {
         Category::create($request->validated());
+        return "ok";
+    }
+
+    public function show($id)
+    {
+        $category = Category::findOrFail($id);
+        return $category;
+    }
+
+    public function update($id, Request $request)
+    {
+        $category = Category::findOrFail($id);
+
+        $category->update(['name' => $request->name]);
+
+        return "ok";
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
         return "ok";
     }
 }
