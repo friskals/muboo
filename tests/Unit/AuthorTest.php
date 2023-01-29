7dbe5693b84025031c171c6b46199f3e77ccd516
Friska Sianturi <friska.lsianturi@gmail.com>
2023-01-29 17:23:38 +0700

    test: add unit test for update author

--- tests/Unit/AuthorTest.php
+++ tests/Unit/AuthorTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use App\Models\Author;
 use Tests\TestCase;
 
+
 class AuthorTest extends TestCase
 {
     private const ENDPOINT = '/admin/authors';
@@ -35,4 +36,22 @@ class AuthorTest extends TestCase
         
         $response->assertStatus(200);
     }
+
+    public function test_update_author_success(){
+        $author = Author::factory()->create();
+
+        $new_name = 'Friska update';
+
+        $response = $this->put(self::ENDPOINT . '/' . $author->id, ['name'=>$new_name]);
+        
+        $response->assertStatus(200);
+
+        $author->refresh();
+
+
+        $this->assertEquals($new_name, $author->name);
+        
+        $author->delete();
+    }
+   
 }
