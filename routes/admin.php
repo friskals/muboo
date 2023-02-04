4b4af47f5c3866663223dc4cf9cfe031f445d803
Friska Sianturi <friska.lsianturi@gmail.com>
2023-02-04 15:54:16 +0700

    fix: route not found

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\Admin\AuthorController;
+use App\Http\Controllers\Admin\BookController;
 use App\Http\Controllers\admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::resources([
     'categories' => CategoryController::class,
-    'authors'=> AuthorController::class
-]);
+    'authors'=> AuthorController::class,
+    'books'=> BookController ::class
+]); 
