9afc1e7721c3c443eedc2ce3666a85be0ed9f261
friskals <friskasianturi23@gmail.com>
2023-02-07 07:04:27 +0700

    feat: update book with image

--- app/Http/Controllers/admin/BookController.php
+++ app/Http/Controllers/admin/BookController.php
@@ -6,10 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Book\StoreRequest;
 use App\Http\Requests\Admin\Book\UpdateRequest;
 use App\Models\Book;
+use Illuminate\Support\Facades\Storage;
 
 class BookController extends Controller
 {
-    public function store(StoreRequest $request){  
+    public function store(StoreRequest $request)
+    {
         $image = $request->file('image');
 
         Book::create([
@@ -23,19 +25,29 @@ class BookController extends Controller
                 'public'
             )
         ]);
-        
+
         return "ok";
     }
 
-    public function update(UpdateRequest $request, $id){
+    public function update(UpdateRequest $request, $id)
+    {
         $book = Book::findOrFail($id);
-        
+
         $updated = $request->all();
 
-        unset($updated['image']);
+        if ($image = $request->file('image')) {
+
+            $updated['image'] = $image->storeAs(
+                'images',
+                $image->getClientOriginalName(),
+                'public'
+            );
+
+            Storage::disk('public')->delete($book->image);
+        }
 
         $book->update($updated);
-        
+
         return "ok";
     }
 }
