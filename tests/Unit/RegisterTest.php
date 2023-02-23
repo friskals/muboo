fc9e27759f97d3479e3c600964f028176a0f92a7
friskals <friskasianturi23@gmail.com>
2023-02-24 06:24:10 +0700

    test: change assertion for register

--- tests/Unit/RegisterTest.php
+++ tests/Unit/RegisterTest.php
@@ -8,18 +8,17 @@ use Tests\TestCase;
 class RegisterTest extends  TestCase
 {
     use RefreshDatabase;
-    // use Exc
+
     public function test_register_user_success()
     {
         $this->withoutExceptionHandling();
-        
-        $response = $this->post('/register',[
-            'name'=>'friskals',
+
+        $this->post('/register', [
+            'name' => 'friskals',
             'email' => "friskasianturi23@gmail.com",
             'password' => 'Apapap123**',
             'password_confirmation' => 'Apapap123**'
-        ]);    
-
-        $response->assertStatus(200);
+        ])
+            ->assertRedirect('/');
     }
 }
