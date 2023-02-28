34cbda86d71821193c22f67db17ce28c04f36a25
friskals <friskasianturi23@gmail.com>
2023-03-01 05:52:05 +0700

    fix: hash password when register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-
         $request = $request->validated();
+        
+        $request['password'] = Hash::make($request['password']);
 
         $user = User::create($request);
 
