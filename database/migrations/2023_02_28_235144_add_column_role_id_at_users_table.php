a093774884a5224c8b834e535d1b07db40267a74
friskals <friskasianturi23@gmail.com>
2023-03-01 07:18:41 +0700

    feat: add middleware admin

---
+++ database/migrations/2023_02_28_235144_add_column_role_id_at_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private $table = 'users';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->table, function($table) {
+            $table->integer('role_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->table, function($table) {
+            $table->dropColumn('role_id');
+        });
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\GeneralPageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -88,3 +89,4 @@ Route::post('/logout', [LogoutController::class, 'perform'])->name('logout');
 
 Route::post('/login',[LoginController   ::class,'login'])->name('login');
 
+Route::get('/unauthorized', [GeneralPageController::class, ['returnPage401']])->name('unauthorized');
