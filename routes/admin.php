08e48be826efc98734561cec292878e3d2f16996
friskals <friskasianturi23@gmail.com>
2023-03-01 07:19:01 +0700

    feat: protect admin feature with middleware

--- routes/admin.php
+++ routes/admin.php
@@ -5,8 +5,11 @@ use App\Http\Controllers\Admin\BookController;
 use App\Http\Controllers\admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::resources([
-    'categories' => CategoryController::class,
-    'authors'=> AuthorController::class,
-    'books'=> BookController ::class
-]); 
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resources([
+        'categories' => CategoryController::class,
+        'authors' => AuthorController::class,
+        'books' => BookController::class
+    ]);
+});
