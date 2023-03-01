e86a5be01fd1d1e2e8359174f9e613fbce352cd5
friskals <friskasianturi23@gmail.com>
2023-03-01 07:19:22 +0700

    test: add tc unauthorized user create category

--- tests/Unit/CategoryTest.php
+++ tests/Unit/CategoryTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -17,28 +18,30 @@ class CategoryTest extends TestCase
      */
     public function test_create_category_success()
     {
-        $response = $this->post('/admin/categories',['name'=>'Science']);    
+        $response = $this->post('/admin/categories', ['name' => 'Science']);
 
         $response->assertStatus(200);
     }
 
-    public function test_show_category_success(){
+    public function test_show_category_success()
+    {
         $category = Category::factory()->create();
 
         $response = $this->get('/admin/categories/' . $category->id);
 
         $category->delete();
-        
+
         $response->assertStatus(200);
     }
 
-    public function test_update_category_success(){
+    public function test_update_category_success()
+    {
 
         $category = Category::factory()->create();
 
         $name_update = 'Science update';
 
-        $response = $this->put('/admin/categories/'.$category->id, ['name'=> $name_update]);
+        $response = $this->put('/admin/categories/' . $category->id, ['name' => $name_update]);
 
         $category->refresh();
 
@@ -49,7 +52,8 @@ class CategoryTest extends TestCase
         $category->delete();
     }
 
-    public function test_delete_category_success(){
+    public function test_delete_category_success()
+    {
         $category = Category::factory()->create();
 
         $response = $this->delete('/admin/categories/' . $category->id);
@@ -57,7 +61,20 @@ class CategoryTest extends TestCase
         $response->assertStatus(200);
 
         $this->assertDatabaseMissing('categories', ['id' => $category->id, 'name' => $category->name]);
+    }
 
+    public function test_unauthorized_cannot_create_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $user =  new User();
+        $user->name = 'unauthorize_user';
+        $user->id = 1;
+        $user->email = "user@testing.com";
+
+        $response = $this->be($user)->post('/admin/categories', ['name' => 'Science']);
+
+        $response->assertRedirect('/unauthorized');//>assertStatus(401);
 
     }
 }
