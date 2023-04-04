75b0469c0821d3aa150a6fe330c3872ddb50fe93
friskals <friskasianturi23@gmail.com>
2023-04-04 07:25:02 +0700

    fix: route not found as overwritten by resource

--- routes/admin.php
+++ routes/admin.php
@@ -8,11 +8,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'web'], function () {
     Route::group(['middleware' => 'admin'], function(){
+        Route::post('books/filter-book', [BookController::class, 'filter'])->name('books.filter');
+        Route::put('books/update-status', [BookController::class, 'updateStatus'])->name('books.update.status');
         Route::resources([
             'categories' => CategoryController::class,
             'authors' => AuthorController::class,
             'books' => BookController::class
         ]); 
-        Route::post('books/filter-book', [BookController::class, 'filter'])->name('books.filter');
     });
 }); 
