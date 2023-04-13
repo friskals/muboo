377c269faa5f9a010ab52e4df389ca15b986fd3f
friskals <friskasianturi23@gmail.com>
2023-04-13 07:24:06 +0700

    feat: display book created from admin

--- app/Http/Controllers/Frontsite/BookController.php
+++ app/Http/Controllers/Frontsite/BookController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers\Frontsite;
 
 use App\Http\Controllers\Controller;
+use App\Models\Book;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
+    const NUMBER_OF_ITEM_PER_PAGE = 5;
+
     public function index(){
+        $books = Book::published()->paginate(self::NUMBER_OF_ITEM_PER_PAGE);
 
         return view('frontsite.index',[
             'isMenu' => true,
-            'isNavbar' => false
+            'isNavbar' => false,
+            'books' => $books
         ]);
     }
 }
--- resources/views/frontsite/index.blade.php
+++ resources/views/frontsite/index.blade.php
@@ -8,35 +8,21 @@
 
 @section('content')
 <h4 class="fw-bold py-3 mb-4">What's new?</h4>
-<div class="row mb-3">
-    {{-- <div class="col-md-6 col-lg-4 mb-3"> --}}
-    <div class="col-sm-3"></div>
-    <div class="col-sm-4">
-        <div class="card h-100">
-          <img class="card-img-top" src="{{asset('assets/img/elements/2.jpg')}}" alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </p>
-            <a href="javascript:void(0)" class="btn btn-outline-primary">Go somewhere</a>
-          </div>
+ @foreach ($books as $book)
+    <div class="row mb-3">
+        <div class="col-sm-3"></div>
+        <div class="col-md-6 col-lg-4 mb-3">
+            <div class="card h-100">
+            <img class="card-img-top" src="{{asset('assets/img/elements/2.jpg')}}" alt="Card image cap" />
+            <div class="card-body">
+                <h5 class="card-title">{{$book->title}}</h5>
+                <p class="card-text">
+                {{$book->excerpts}}
+                </p>
+                <a href="javascript:void(0)" class="btn btn-outline-primary">Go somewhere</a>
+            </div>
+            </div>
         </div>
-      </div>
-  </div>
-  <div class="row mb-3">
-    <div class="col-sm-3"></div>
-    <div class="col-md-6 col-lg-4 mb-3">
-      <div class="card h-100">
-        <img class="card-img-top" src="{{asset('assets/img/elements/2.jpg')}}" alt="Card image cap" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-          </p>
-          <a href="javascript:void(0)" class="btn btn-outline-primary">Go somewhere</a>
-        </div>
-      </div>
     </div>
-  </div>
+ @endforeach
 @endsection
