f9913de50b66a8acef22b8beca1557826884b195
friskals <friskasianturi23@gmail.com>
2023-04-18 19:04:19 +0700

    test: add script test for api add book's music

---
+++ database/factories/MusicFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BookAuthor;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class MusicFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $user = User::factory()->create();
+
+        $bookAuthor = BookAuthor::factory()->create();
+        
+        return [
+            
+        ];
+    }
+}
--- tests/Unit/MusicTest.php
+++ tests/Unit/MusicTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\Unit;
 
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\TestCase;
 use Tests\TestCase as TestsTestCase;
 
 class MusicTest extends TestsTestCase
 {
-    private const ENDPOINT = 'api/music/search';
+    use RefreshDatabase;
+
     /**
      * A basic unit test example.
      *
@@ -15,10 +19,45 @@ class MusicTest extends TestsTestCase
      */
     public function test_search_music_success()
     {
-        $request = ['q' => 'queen i want to break free'];
+        $uri = '/music/search';
 
-        $response = $this->get(self::ENDPOINT."?q=Joji die for you");
+        $response = $this->get($uri . "?q=Joji die for you");
 
         $response->assertStatus(200);
-    }   
+    }
+
+    public function test_add_my_songs_to_a_book()
+    {
+        $user = User::factory()->create();
+
+        $this->signIn($user);
+
+        $book = Book::factory()->create();
+
+        $request = [
+            'book_id' => $book->id,
+            'external_music_id' => 'abc',
+            'title' => 'music title'
+        ];
+
+        $this->post('book/music', $request)->assertStatus(200)
+            ->assertJsonStructure(['success', 'data']);
+
+        $this->assertDatabaseHas('musics', [
+            'book_id' => $book->id,
+            'external_music_id' => 'abc',
+            'title' => 'music title'
+        ]);
+
+
+        $this->assertDatabaseHas('music_fans', [
+            'music_id' => $book->id,
+            'user_id' => $user->id
+        ]);
+    }
+
+    public function test_get_particular_music_for_book()
+    {
+        $book = Book::factory()->create();
+    }
 }
