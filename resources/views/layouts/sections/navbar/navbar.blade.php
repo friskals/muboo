26ee9426f2903ba9d57f9ceded397e816e67539b
friskals <friskasianturi23@gmail.com>
2023-04-22 15:24:03 +0700

    fix: use user name instead of hardcoded name

--- resources/views/layouts/sections/navbar/navbar.blade.php
+++ resources/views/layouts/sections/navbar/navbar.blade.php
@@ -67,7 +67,7 @@ $navbarDetached = ($navbarDetached ?? '');
                       </div>
                     </div>
                     <div class="flex-grow-1">
-                      <span class="fw-semibold d-block">John Doe</span>
+                      <span class="fw-semibold d-block">{{auth()->user()->name}}</span>
                       <small class="text-muted">Admin</small>
                     </div>
                   </div>
